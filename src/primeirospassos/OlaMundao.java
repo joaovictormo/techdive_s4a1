1dd08c7e1972bfb739703edc757a3b0cbfadf00c
João Victor <jvmdeoliveira@gmail.com>
2021-11-22 21:09:47 -0300

    adiciona gitignore e classe main

---
+++ src/primeirospassos/OlaMundao.java
@@ -0,0 +1,32 @@
+package primeirospassos;
+
+public class OlaMundao {
+    public static void main(String[] args) {
+        System.out.println("Olá, turma do Tech Dive!");
+        char letra = 'J';
+        char outraLetra;
+        outraLetra = 'V';
+        System.out.println(letra);
+        System.out.println(outraLetra);
+        outraLetra = 'O';
+        System.out.println(outraLetra);
+        outraLetra = 2.5; // errado
+        int idade = 30;
+        idade = "asdasdassdadsa"; // errado
+        double $salario = 20.5;
+        int _idade = 30;
+        int idade2 = 30;
+        String cabecalho = "Título da página";
+        double void = 30.6; // errado
+        int idadeDoUsuario;
+        String nomeDaMae;
+
+        nomeDaMae = "Marta";
+
+        idadeDoUsuario = 35;
+
+        final double PI = 3.14;
+
+        PI = 3.1415;
+    }
+}
